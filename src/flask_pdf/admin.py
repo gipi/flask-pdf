1b1da8b8782e9344f6bb1e73df55a15ba5283880
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-05-17 10:32:49 +0200

    Add the PDFPages field to the admin.

--- src/flask_pdf/admin.py
+++ src/flask_pdf/admin.py
@@ -1,11 +1,33 @@
-from flask_peewee.admin import Admin, ModelAdmin
+from flask_peewee.admin import Admin, ModelAdmin, AdminModelConverter
 
 from . import app, db
 from .auth import auth
-from .models import User, PDF
+from .models import User, PDF, PagesField
+from wtforms import fields
+
+class PagesFormField(fields.TextField):
+    def process_data(self, value):
+        "Here we have model data"
+        self.data = ','.join([str(x) for x in value])
+
+    def process_formdata(self, valuelist):
+        self.data = valuelist[0].split(',')
+
+class PDFPagesConverter(AdminModelConverter):
+    def __init__(self, model_admin, **kwargs):
+        super(PDFPagesConverter, self).__init__(model_admin, **kwargs)
+        self.converters[PagesField] = self.handle_pages
+
+    def handle_pages(self, model, field, **kwargs):
+        return field.name, PagesFormField(**kwargs)
 
 class PDFAdmin(ModelAdmin):
     filter_exclude = ['pages',]
+    columns = (
+        'title',
+        'pages',
+    )
+    form_converter = PDFPagesConverter
 
 admin = Admin(app, auth)
 auth.register_admin(admin)
